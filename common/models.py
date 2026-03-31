2cba5bdb7de2f1502e34f407655c4f13c242ab46
Elias McCloud <emccloud60187@gmail.com>
2026-03-31 15:46:45 -0500

    Added a client controlled main match column for the masking condition in fuzzy. Added a model validator for the main match column

--- common/models.py
+++ common/models.py
@@ -1,6 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
 from pathlib import Path
-from typing import Optional,Union
+from typing import Optional
+
+from common.exceptions import ConfigError
 
 
 class ColumnTypeConfig(BaseModel):
@@ -22,8 +24,26 @@ class ClientConfig(BaseModel):
     FILE_PATH: Path
     COLUMNS:  Columns
     BLOCKING: str
+    MAIN_MATCH_CRITERIA: str
     NICKNAME: Optional[str] = None
     BOUNDS: Optional[list[dict[str,float]]] = None
+    
+    @model_validator(mode='after')
+    def validate_main_match_criteria(self):
+        allowed = []
+        for field_name,field_value in self.COLUMNS:
+            if field_value is not None:
+                if field_name in ['phone', 'email']:
+                    allowed.extend(field_value.columns)
+                elif field_name == 'name':
+                    allowed.extend([col for col,_ in field_value.weight])
+                else:
+                    allowed.append('address')
+        print(allowed)
+        if self.MAIN_MATCH_CRITERIA not in allowed:
+            raise ConfigError(f"The MAIN_MATCH_CRITERIA value must be one of {allowed}.")
+        return self
+
 
 class SystemConfig(BaseModel):
     CLIENT_YAML: Path
