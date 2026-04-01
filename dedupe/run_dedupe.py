5e3f1771c4658e287e5cfa567b541b1aaa3e5369
Elias McCloud <emccloud60187@gmail.com>
2026-03-31 20:56:27 -0500

    created blocking function, and added specific type hints

--- dedupe/run_dedupe.py
+++ dedupe/run_dedupe.py
@@ -1,43 +1,30 @@
 import pandas as pd
 import sys
 
-from pydantic import BaseModel
+
+
+from dedupe.dsu import DSU
+from dedupe.normalize import normalize_df
+from dedupe.core_dedupe import run_strict_dedupe, run_fuzzy_dedupe
 
 from common.exceptions import ConfigError
 from common.utils import load_data_df
-from dedupe.normalize import normalize_df
-from dedupe.core_dedupe import run_strict_dedupe, run_fuzzy_dedupe
 from common.logger import get_logger
+from common.models import ClientConfig, Blocking
 
 logger = get_logger(__name__)
 
 
-class DSU:
-    def __init__(self,n):
-        self.parent = list(range(n))
-        self.rank = [0] * n
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-    def union(self,x,y):
-        root_x = self.find(x)
-        root_y = self.find(y)
-        if self.rank[root_x] < self.rank[root_y]:
-            self.parent[root_x] = root_y
-        elif self.rank[root_x] > self.rank[root_y]:
-            self.parent[root_y] = root_x
-        else:
-            self.parent[root_y] = root_x
-            self.rank[root_x] += 1  
+
         
 
-def column_weights(cleaned_cols: list[str], client_cfg: object) -> dict:
+def column_weights(cleaned_cols: list[str], client_cfg: ClientConfig) -> dict:
     # Getting the column weights from the client cfg file, combining them with the dataframe column names that will be used for the fuzzy
     # Separate dictionary unioned in becuase the structure of the name weights in the cfg file is different to allow for weights per name column
     return {col: getattr(client_cfg.COLUMNS,col.split(':')[1].strip()).weight for col in cleaned_cols} | {name_col:weight for name_col,weight in client_cfg.COLUMNS.name.weight}
     
 
+# Gives user choice to auto-balance or exit. THIS FUNCTION CAN EXIT THE PROGRAM
 def test_weights(weighted_cols: dict):
 
     total_weight = sum(weighted_cols.values())
@@ -75,60 +62,82 @@ def test_weights(weighted_cols: dict):
     return weighted_cols
  
 
+def create_block(main_df: pd.DataFrame, blocking_rules: Blocking, fuzzy: bool):
+    if fuzzy:
+        dupe_df = main_df[main_df['count'] == 1]
+    else:
+        dupe_df = main_df
+
+
+    if blocking_rules.portion is None or blocking_rules.type.lower() == 'state':
+        return dupe_df.groupby(dupe_df[blocking_rules.column])
+    
+    else:
+        if blocking_rules.portion.lower() == 'start':
+            return dupe_df.groupby(dupe_df[blocking_rules.column].str[:3])
+        else:
+            return dupe_df.groupby(dupe_df[blocking_rules.column].str[-3:])
+        
+
+        
+            
+
 
     
+
+
+
+def run_dedupe(client_cfg: ClientConfig) -> None:
     
-
-
-
-def run_dedupe(client_cfg: BaseModel) -> None:
-    #Load file into df
+    # Load file into df
     original_df = load_data_df(client_cfg.FILE_PATH)
 
-    logger.info("Normalizing the data")
-    #Normalize the df
+
+    # Normalize the df
     normalized_df = normalize_df(original_df, client_cfg.COLUMNS)    
+    dsu = DSU(len(normalized_df))
 
 
-
-    # Only dedupe on normalized columns
+    # Primary columns are normalized columns usually combined with names for strict dedupe
     primary = [col for col in normalized_df.columns if col.endswith((':name_email',':name_phone',':name_address'))]
+    # Secondary columns are non name combined normalized columns for fuzzy
     secondary = [col for col in normalized_df.columns if col.endswith((':email',':phone',':address'))]
     cols = primary if primary else secondary
     
 
-    # Instantiate DSU 
-    dsu = DSU(len(normalized_df))
+    blocks = create_block(normalized_df, client_cfg.BLOCKING, False)
+    print(len(blocks))
+    sys.exit()
 
-    logger.info("Running strict dedupe")
+
     # Run strict dedupe
-    main_df = run_strict_dedupe(normalized_df, cols, dsu)
+    main_df = run_strict_dedupe(normalized_df, cols, dsu, blocks)
+
     
+    # Handle column set up for fuzzy
     cols = secondary
-    
     # Create dictionary for column weights. Specified in client config
     weighted_cols = column_weights(cols,client_cfg)
 
-   
     # Ensure given weights add to 1.0
-    # Gives user choice to auto-balance or exit. THIS FUNCTION CAN EXIT THE PROGRAM
     weighted_cols = test_weights(weighted_cols)
 
+    blocks = create_block(main_df, client_cfg.BLOCKING, True)
 
-    
     # Run fuzzy dedupe
-    logger.info("Running fuzzy dedupe")
     main_df = run_fuzzy_dedupe(main_df=main_df, 
                                cols=weighted_cols, 
                                dsu=dsu, 
-                               blocking=client_cfg.BLOCKING, 
+                               blocks=blocks,
                                u_bound=client_cfg.BOUNDS.u_bound, 
                                l_bound=client_cfg.BOUNDS.l_bound, 
                                main_match_criteria=client_cfg.MAIN_MATCH_CRITERIA, 
                                nickname_col=client_cfg.NICKNAME)
     
-    logger.info("Assigning matching ids")
+
+
+
     main_df['root'] = main_df.index.map(dsu.find)
     main_df['match_id'] = main_df['root'].map(main_df['legacycontactid'])
-    main_df.sort_values('match_id').to_csv('after_fuzzy.csv',index=False)
+    main_df.sort_values('match_id').to_csv('newest_fuzzy.csv',index=False)
     logger.info("Dedupe complete")
