2ba77da80f28cd5ba88022cf2aa1d375d6e72155
Elias McCloud <emccloud60187@gmail.com>
2026-04-02 17:42:14 -0500

    Removed load system config function as its no longer used

--- common/utils.py
+++ common/utils.py
@@ -5,37 +5,29 @@ from pydantic import ValidationError
 
 from common.logger import get_logger
 from common.exceptions import DataLoadError, ConfigError
-from common.models import ClientConfig, SystemConfig
+from common.models import ClientConfig
+
 logger = get_logger(__name__)
 
-# Load config for program with client YAML file path
-def load_sys_config(file_path: str) -> dict:
-    
-    try:
-        with open(file_path, 'r') as f:
-            config = yaml.safe_load(f)
-    except DataLoadError as e:
-        raise DataLoadError(f"Failed to load program config file: {file_path}") from e
-    try:
-        system_settings = SystemConfig(**config)
-    except ValidationError as e:
-        raise ConfigError(f"Invalid program configuration. Check file for labeling errors: {file_path}") from e
-    return system_settings
-    
 
 # Transform client YAML to Pydantic model Settings
-def load_client_config(file_path: str):
+def load_client_config(file_path: str) -> dict:
+    if not file_path.endswith((".yaml", ".yml")):
+        raise DataLoadError(f"File path is not a yaml file. Please try again")
     try:
-        with open(file_path, 'r') as f:
+        with open(file_path, "r") as f:
             raw_config = yaml.safe_load(f)
-    except DataLoadError as e:
+    except (FileNotFoundError, OSError) as e:
         raise DataLoadError(f"Failed to load client config file: {file_path}") from e
     try:
         client_settings = ClientConfig(**raw_config)
         logger.info(f"{client_settings.CLIENT_NAME} config loaded")
     except ValidationError as e:
-        raise ConfigError(f"Invalid client configuration. Check file for labeling errors: {file_path}") from e
+        raise ConfigError(
+            f"Invalid client configuration. Check file for labeling errors: {file_path}"
+        ) from e
     return client_settings
+
 
 def load_data_df(file_path: str) -> pd.DataFrame:
     if file_path.suffix == ".csv":
@@ -43,10 +35,10 @@ def load_data_df(file_path: str) -> pd.DataFrame:
             df = pd.read_csv(file_path)
         except Exception as e:
             raise DataLoadError(f"Failed to load csv data file: {file_path}") from e
-            
+
     else:
         try:
-            df = pd.read_excel(file_path, engine='openpyxl')
+            df = pd.read_excel(file_path, engine="openpyxl")
         except Exception as e:
             raise DataLoadError(f"Failed to load xlsx data file: {file_path}") from e
     return df
