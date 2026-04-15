3f0a59a67fe2ca01a73d98631f740d2974c8c785
Elias McCloud <emccloud60187@gmail.com>
2026-04-15 11:51:29 -0500

    Updated how the yaml structure works. Only one Column type config for all contact types which includes name. User can now assign weights to each specific field in a contact type, or just assign one weight for the entire contact type which will be evenly split between all fields within that contact type

--- common/models.py
+++ common/models.py
@@ -8,20 +8,15 @@ from common.exceptions import ConfigError
 
 class ColumnTypeConfig(BaseModel):
     include_name: bool = False
-    weight: Optional[float] = 0.0
+    weight: list[tuple[str, float]] | float = 0.0
     columns: list[str] = []
-
-
-class NameColumnTypeConfig(BaseModel):
-    columns: list[str] = []
-    weight: list[tuple[str, float]]
 
 
 class Columns(BaseModel):
     phone: Optional[ColumnTypeConfig] = None
     email: Optional[ColumnTypeConfig] = None
     address: Optional[ColumnTypeConfig] = None
-    name: Optional[NameColumnTypeConfig] = None
+    name: Optional[ColumnTypeConfig] = None
 
 
 class Blocking(BaseModel):
@@ -50,10 +45,8 @@ class ClientConfig(BaseModel):
         allowed = []
         for field_name, field_value in self.COLUMNS:
             if field_value is not None:
-                if field_name in ["phone", "email"]:
+                if field_name in ["phone", "email", "name"]:
                     allowed.extend(field_value.columns)
-                elif field_name == "name":
-                    allowed.extend([col for col, _ in field_value.weight])
                 else:
                     allowed.append("address")
         if self.MAIN_MATCH_CRITERIA not in allowed:
