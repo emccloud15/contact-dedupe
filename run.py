aaa6bb814a333cf4a4afcd0f195059ad498e5f37
Elias McCloud <emccloud60187@gmail.com>
2026-05-21 14:29:46 -0500

    Updated cli input to be able to handle one directory containing the yaml and dupe file

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import argparse
 
 from pathlib import Path
 
-from common.utils import load_client_config
+from common.utils import load_client_config, load_data_from_dir
 from common.logger import get_logger
 from dedupe.run_dedupe import run_dedupe
 from common.exceptions import DataLoadError, ConfigError
@@ -13,26 +13,39 @@ logger = get_logger(__name__)
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Dedupe Tool")
-    parser.add_argument(
-        "--yaml", "-y", type=str, required=True, help="Input client yaml path"
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        "--dir", "-d", type=str, help="Directory that includes the yaml and duplicated file. Output will be located here."
     )
-    parser.add_argument(
-        "--input", "-i", type=str, required=True, help="File path to be deduplicated"
+    group.add_argument(
+        "--yaml", "-y", type=str, help="Input client yaml path"
     )
-    parser.add_argument(
-        "--output", "-o", type=str, required=True, help="Directory for output files"
+    group.add_argument(
+        "--dupe", type=str, help="File path to be deduplicated"
     )
+    group.add_argument(
+        "--output", "-o", type=str, help="Directory for output files"
+    )
+    
     return parser.parse_args()
 
 
 def main():
+
     args = parse_args()
-    input_path = Path(args.input)
-    output_path = Path(args.output)
-    output_path.mkdir(parents=True, exist_ok=True)
+    
     try:
-        client_config = load_client_config(args.yaml)
-        run_dedupe(client_config, input_path, output_path)
+        if args.dir:
+            output_path = Path(args.dir)
+            output_path.mkdir(parents=True, exist_ok=True)
+            yaml_file, dupe_file = load_data_from_dir(output_path)
+        else:
+            yaml_file = args.yaml
+            dupe_file = Path(args.dupe)
+            output_path = Path(args.output)
+            output_path.mkdir(parents=True, exist_ok=True)
+        client_config = load_client_config(yaml_file)
+        run_dedupe(client_config, dupe_file, output_path)
     except DataLoadError as e:
         logger.exception(f"Dedupe failed during loading data: {e}")
         sys.exit(1)
