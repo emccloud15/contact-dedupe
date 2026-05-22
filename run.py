7dfab275214856de23141862dfefaa80273e594d
Elias McCloud <emccloud60187@gmail.com>
2026-05-22 17:52:10 -0500

    Changed the parser to optional. This will need to be fixed

--- run.py
+++ run.py
@@ -13,17 +13,16 @@ logger = get_logger(__name__)
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Dedupe Tool")
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument(
+    parser.add_argument(
         "--dir", "-d", type=str, help="Directory that includes the yaml and duplicated file. Output will be located here."
     )
-    group.add_argument(
+    parser.add_argument(
         "--yaml", "-y", type=str, help="Input client yaml path"
     )
-    group.add_argument(
+    parser.add_argument(
         "--dupe", type=str, help="File path to be deduplicated"
     )
-    group.add_argument(
+    parser.add_argument(
         "--output", "-o", type=str, help="Directory for output files"
     )
     
@@ -40,7 +39,7 @@ def main():
             output_path.mkdir(parents=True, exist_ok=True)
             yaml_file, dupe_file = load_data_from_dir(output_path)
         else:
-            yaml_file = args.yaml
+            yaml_file = Path(args.yaml)
             dupe_file = Path(args.dupe)
             output_path = Path(args.output)
             output_path.mkdir(parents=True, exist_ok=True)
